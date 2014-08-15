eaa86a7336f56d3138eeb53e42be1ace2b7f9c7a
JamesCWolff <jcw6@njit.edu>
2014-08-15 19:34:29 -0400

    Added GameCharacter class, please fill in attack and attacked functions

---
+++ GameCharacter.java
@@ -0,0 +1,13 @@
+public class GameCharacter{
+	int hp;
+	int def;
+	int str;
+	
+	public void Attack(GameCharacter target, int damage){
+		//Attack code goes here
+	}
+	
+	public void Attacked(int damage){
+		//Attacked code goes here
+	}
+}
