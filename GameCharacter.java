ab73b7a7468d02cc6055144c9ec704613d6b89fa
Will <xw0lffx@gmail.com>
2014-08-20 17:06:26 -0400

    added functions to game charecter

--- GameCharacter.java
+++ GameCharacter.java
@@ -1,4 +1,7 @@
+package JavaDemo;
+
 public class GameCharacter{
+	String name;
 	//hp is the health variable
 	int hp;
 	//def is the defense variable
@@ -6,11 +9,39 @@ public class GameCharacter{
 	//str is the strength variable
 	int str;
 	
+	public void Attack(GameCharacter target){
+		//Attack code goes here
+		target.Attacked(str);
+		
+	}
 	public void Attack(GameCharacter target, int damage){
 		//Attack code goes here
+		target.Attacked(damage);
+		
 	}
 	
-	public void Attacked(int damage){
+	public void SetCharecter(String n, int h, int d, int s)
+	{
+		name = n;
+		hp = h;
+		def = d;
+		str = s;
+		
+	}
+	
+	public void Attacked(int damage)
+	{
 		//Attacked code goes here
+		if(def <= damage)
+			hp = hp - (damage - def);
+		
+		if (hp <= 0)
+		{
+			Dead();
+		}
+	}
+	void Dead()
+	{
+		System.out.println(name + " has been defeated!");
 	}
 }
--- MainWindow.java
+++ MainWindow.java
@@ -1,3 +1,5 @@
+package JavaDemo;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -37,11 +39,21 @@ public class MainWindow extends JFrame{
 	}
 	
 	public static void main(String[]args){
-		MainWindow gui = new MainWindow();
+		/*MainWindow gui = new MainWindow();
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gui.setSize(300,100);
 		//gui.setLocation(300,300);
 		gui.setVisible(true);
-		gui.setTitle("Main Window");
+		gui.setTitle("Main Window");*/
+		
+		GameCharacter player = new GameCharacter();
+		GameCharacter enemy = new GameCharacter();
+		
+		player.SetCharecter("James", 100, 8, 70);
+		enemy.SetCharecter("Slime", 69, 2, 10);
+		
+		player.Attack(enemy);
+		enemy.Attack(player, 1000);
+		player.Attack(enemy);
 	}
 }
